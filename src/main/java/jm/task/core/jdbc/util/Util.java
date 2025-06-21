594df8946b862e83584760b71358ea22241e39de
MaksimAvAvAv <shytazellavavav@gmail.com>
2025-06-21 13:28:07 +0500

    task 1.1.4Ð

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,8 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,6 +11,10 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
     private static final String USER = "avavav";
     private static final String PASSWORD = "2296810";
+
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -18,5 +25,35 @@ public class Util {
             System.err.println("Ошибка при подключении к БД: " + e.getMessage());
         }
         return connection;
+    }
+
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USER);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+
+
+            configuration.addAnnotatedClass(jm.task.core.jdbc.model.User.class);
+
+            return configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+
+    public static void shutdown() {
+        getSessionFactory().close();
     }
 }
